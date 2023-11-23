43d65b5dc7d5f3a17a041fbd3832179507cca74d
VQA_IoT <82134572+mitsumi73@users.noreply.github.com>
2023-11-23 21:51:50 +0900

    Update Telebot2.py

--- src/Telebot2.py
+++ src/Telebot2.py
@@ -1,29 +1,23 @@
 import sched
 import time
-import asyncio
 from datetime import datetime
 import pytz
 from dotenv import load_dotenv
 import telebot
 import os
 
-
 load_dotenv()
 TOKEN = os.getenv('TELEGRAM_API_KEY')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 bot = telebot.TeleBot(TOKEN)
-bot.send_message(CHAT_ID, "Hello")
 scheduler = sched.scheduler(time.time, time.sleep)
 
 def send_message(chat_id):
     now = datetime.now(pytz.timezone('Asia/Seoul'))
     time_str = now.strftime("%H:%M")
     message = f"Current time: {time_str}"
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(bot.send_message(chat_id, message))
-    loop.close()
+    bot.send_message(chat_id, message)
 
 def scheduler_job():
     now = datetime.now(pytz.timezone('Asia/Seoul'))
