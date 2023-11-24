632b45967b6c47edc26d895820b9b28c0f6c1014
VQA_IoT <82134572+mitsumi73@users.noreply.github.com>
2023-11-24 13:00:49 +0900

    Update Telebot2.py

--- src/Telebot2.py
+++ src/Telebot2.py
@@ -13,17 +13,25 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 bot = telebot.TeleBot(TOKEN)
 scheduler = sched.scheduler(time.time, time.sleep)
 
+
 def send_message(chat_id):
     now = datetime.now(pytz.timezone('Asia/Seoul'))
     time_str = now.strftime("%H:%M")
     message = f"Current time: {time_str}"
     bot.send_message(chat_id, message)
 
+
 def scheduler_job():
     now = datetime.now(pytz.timezone('Asia/Seoul'))
     if 23 > now.hour >= 6:
         send_message(CHAT_ID)
     scheduler.enter(1800, 1, scheduler_job)
+
+
 while True:
     scheduler.enter(0, 1, scheduler_job)
-    scheduler.run()
+    try:
+        scheduler.run()
+    except KeyboardInterrupt:
+        print("Program interrupted by user.")
+        # Include any cleanup or resource release code here
