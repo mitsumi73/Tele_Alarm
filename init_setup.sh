3f4f770672c3f38df5d1fa16361ebad1b39c939a
VQA_IoT <82134572+mitsumi73@users.noreply.github.com>
2023-11-24 20:17:22 +0900

    Updates

--- init_setup.sh
+++ init_setup.sh
@@ -1,6 +1,6 @@
 echo [$(date)]: "START"
-echo [$(date)]: "Creating conda env with python 3.9"
-conda create --prefix ./env python=3.9 -y
+echo [$(date)]: "Creating conda env with python 3.8"
+conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activate env"
 source activate ./env
 echo [$(date)]: "installing the requirements"
